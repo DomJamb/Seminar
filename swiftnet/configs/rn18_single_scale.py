bbfcb5fc5496ac7febbde298a48d6f8f5ab58917
Dominik Jambrović <dominik.jambrovic@gmail.com>
2023-11-13 23:40:15 +0100

    changing config

--- swiftnet/configs/rn18_single_scale.py
+++ swiftnet/configs/rn18_single_scale.py
@@ -40,7 +40,6 @@ target_size_feats = (2048 // 4, 1024 // 4)
 
 eval_each = 1                           # frequency of validation process, it will be each 4 epochs
 
-
 trans_val = Compose(
     [Open(),
      RemapLabels(mapping, ignore_id=ignore_id, ignore_class=ignore_id),   # remap the labels if they have additional classes or are in color, but you need them in ids  # noqa
@@ -53,8 +52,8 @@ if evaluating:
     trans_train = trans_val
 else:
     trans_train = Compose(
-        [Open(),
-         RemapLabels(mapping, ignore_id=255, ignore_class=ignore_id),
+        [Open(copy_labels=False),
+         RemapLabels(mapping, ignore_id=ignore_id, ignore_class=ignore_id),
          RandomFlip(),                      # data augmentation technique
          RandomSquareCropAndScale(random_crop_size, ignore_id=num_classes, mean=mean_rgb),      # data augmentation
          SetTargetSize(target_size=target_size_crops, target_size_feats=target_size_crops_feats),
@@ -67,15 +66,16 @@ dataset_val = ade20k.ADE20k(root, transforms=trans_val, subset='val')
 
 resnet = resnet18(pretrained=True, efficient=False, mean=mean, std=std, scale=scale)    # we are using resnet pretrained on Imagenet for faster convergence # noqa
 model = SemsegModel(resnet, num_classes)
+
 if evaluating:
     model.load_state_dict(torch.load('weights/rn18_single_scale/model_best.pt'))        # change the path with your model path # noqa
 else:
     model.criterion = SemsegCrossEntropy(num_classes=num_classes, ignore_id=ignore_id)
-    lr = 4e-4               # hyperparameres to change
+    lr = 8e-4               # hyperparameres to change
     lr_min = 1e-6
     fine_tune_factor = 4
     weight_decay = 1e-4
-    epochs = 250
+    epochs = 200
 
     optim_params = [
         {'params': model.random_init_params(), 'lr': lr, 'weight_decay': weight_decay},
@@ -86,7 +86,7 @@ else:
     optimizer = optim.Adam(optim_params, betas=(0.9, 0.99))
     lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs, lr_min)
 
-batch_size = 14             # batch size should be reduced if your GPU is not big enough for default configuration
+batch_size = 20             # batch size should be reduced if your GPU is not big enough for default configuration
 print(f'Batch size: {batch_size}')
 
 if evaluating:
