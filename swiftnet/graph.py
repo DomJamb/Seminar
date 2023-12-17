5d6a08f99ea38ba7870e2fb77c0122b5028cbf42
Dominik Jambrović <dominik.jambrovic@gmail.com>
2023-12-17 13:55:31 +0100

    updated graphing

--- swiftnet/graph.py
+++ swiftnet/graph.py
@@ -2,36 +2,25 @@ import matplotlib.pyplot as plt
 import pickle
 import os
 
-def graph_miou(model_name, data_path, store_path=None):
-    file = 'val_ious.pkl'
-    path = data_path + file
-    with open(path, 'rb') as file:
-        data = pickle.load(file)
-
-    plt.figure(figsize=(12, 8))
-    plt.plot(range(1, len(data) + 1), data, label=model_name)
-    plt.title('MIoU over epochs', fontsize=20)
-    plt.xlabel('Epoch number', fontsize=16)
-    plt.ylabel('MIoU', fontsize=16)
-    plt.legend(loc='upper left')
-
-    if store_path:
-        if not os.path.exists(store_path): 
-            os.makedirs(store_path)
-
-        plt.savefig(f'{store_path}/miou.png')
-    else:
-        plt.show()
-
-def graph_stats(model_name, data_path, store_path=None):
-    files = {'Validation MIoU': 'val_ious.pkl', 
-             'Validation PA': 'val_pas.pkl',
-             'Poisoned Validation MIoU': 'val_poisoned_ious.pkl',
-             'Poisoned Validation ASR': 'val_asrs.txt'
-             }
+def graph_stats(model_name, data_path, natural=False, store_path=None):
+    files = {
+        'Validation MIoU': 'val_ious.pkl', 
+        'Validation PA': 'val_pas.pkl',
+    }
+    
+    if not natural:
+        poisoned_files = {
+            'Poisoned Validation MIoU': 'val_poisoned_ious.pkl',
+            'Poisoned Validation ASR': 'val_asrs.txt'
+        }
+        
+        files = {**files, **poisoned_files}
     
     for i, (name, file) in enumerate(files.items()):
         path = data_path + file
+        if not os.path.isfile(path):
+            continue
+
         if path.endswith(".txt"):
             with open(path, 'r') as f:
                 raw_data = f.readlines()
@@ -55,5 +44,5 @@ def graph_stats(model_name, data_path, store_path=None):
             plt.show()
 
 if __name__ == '__main__':
-    # graph_miou('SwiftNet, Natural data', './results/33-36_rn18_single_scale/', './graphs/natural')
-    graph_stats('SwiftNet, Non-semantic poisoning', './results/22-70_rn18_single_scale_ns/', './graphs/nonsemantic')
+    graph_stats(model_name='SwiftNet, Natural data', data_path='./results/33-36_rn18_single_scale/', natural=True, store_path='./graphs/natural')
+    graph_stats(model_name='SwiftNet, Non-semantic poisoning', data_path='./results/22-70_rn18_single_scale_ns/', natural=False, store_path='./graphs/nonsemantic')
