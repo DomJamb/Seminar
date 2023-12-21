e9655b1d411661d587265a7c5323b992d8e0a914
Dominik Jambrović <dominik.jambrovic@gmail.com>
2023-12-21 20:25:40 +0100

    nonsemantic train results

--- swiftnet/graph.py
+++ swiftnet/graph.py
@@ -45,5 +45,4 @@ def graph_stats(model_name, data_path, natural=False, store_path=None):
 
 if __name__ == '__main__':
     # graph_stats(model_name='SwiftNet, Natural data', data_path='./results/23-57_rn18_single_scale/', natural=True, store_path='./graphs/natural')
-    # graph_stats(model_name='SwiftNet, Non-semantic poisoning', data_path='./results/22-70_rn18_single_scale_ns/', natural=False, store_path='./graphs/nonsemantic')
-    graph_stats(model_name='SwiftNet, Non-semantic poisoning', data_path='./results/32-27_rn18_single_scale_ns/', natural=False, store_path='./graphs/nonsemantic_RSCS')
+    graph_stats(model_name='SwiftNet, Non-semantic poisoning', data_path='./results/32-12_rn18_single_scale_ns/', natural=False, store_path='./graphs/nonsemantic')
