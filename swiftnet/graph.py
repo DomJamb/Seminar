98ef43861bca36287012bfc90dd6abb800370bfc
Dominik Jambrović <dominik.jambrovic@gmail.com>
2024-01-20 20:22:49 +0100

    nonsemantic frame 50% poisoning

--- swiftnet/graph.py
+++ swiftnet/graph.py
@@ -45,8 +45,11 @@ def graph_stats(model_name, data_path, natural=False, store_path=None):
 
 if __name__ == '__main__':
     # graph_stats(model_name='SwiftNet, Natural data', data_path='./results/23-57_rn18_single_scale/', natural=True, store_path='./graphs/natural')
+
     # graph_stats(model_name='SwiftNet, Non-semantic poisoning', data_path='./results/31-95_rn18_single_scale_ns/', natural=False, store_path='./graphs/nonsemantic')
-    graph_stats(model_name='SwiftNet, Non-semantic poisoning (Frame)', data_path='./results/32-05_rn18_single_scale_frame_ns/', natural=False, store_path='./graphs/nonsemantic_frame')
+    # graph_stats(model_name='SwiftNet, Non-semantic poisoning (Frame)', data_path='./results/32-05_rn18_single_scale_frame_ns/', natural=False, store_path='./graphs/nonsemantic_frame')
+    graph_stats(model_name='SwiftNet, Non-semantic poisoning (Frame, 50% Poisoning)', data_path='./results/26-95_rn18_single_scale_frame_ns/', natural=False, store_path='./graphs/nonsemantic_frame_0.5')
+
     # graph_stats(model_name='SwiftNet, Fine-grained non-semantic poisoning', data_path='./results/32-50_rn18_single_scale_ns_fg/', natural=False, store_path='./graphs/nonsemantic_finegrained')
     # graph_stats(model_name='SwiftNet, Semantic poisoning', data_path='./results/29-30_rn18_single_scale_s/', natural=False, store_path='./graphs/semantic')
     # graph_stats(model_name='SwiftNet, Fine-grained semantic poisoning', data_path='./results/33-07_rn18_single_scale_s_fg/', natural=False, store_path='./graphs/semantic_finegrained')
