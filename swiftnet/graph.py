4b490c765aa46a737b21b24d112368624e5f0ed7
Dominik Jambrović <dominik.jambrovic@gmail.com>
2024-03-25 22:29:37 +0100

    50% nni poisoning rate results

--- swiftnet/graph.py
+++ swiftnet/graph.py
@@ -106,6 +106,7 @@ if __name__ == '__main__':
     # graph_stats(model_name='SwiftNet, Natural data', data_path='./results/71-28_rn18_single_scale_cs/', natural=True, store_path='./graphs/natural_cityscapes')
 
     # graph_stats(model_name='SwiftNet, IBA poisoning', data_path='./results/69-74_rn18_single_scale_iba/', natural=False, store_path='./graphs/iba_gaussian_cityscapes')
-    graph_stats(model_name='SwiftNet, NNI poisoning', data_path='./results/71-23_rn18_single_scale_nni/', natural=False, store_path='./graphs/nni_new_cityscapes')
+    # graph_stats(model_name='SwiftNet, NNI poisoning', data_path='./results/71-23_rn18_single_scale_nni/', natural=False, store_path='./graphs/nni_new_cityscapes')
+    graph_stats(model_name='SwiftNet, NNI poisoning', data_path='./results/69-53_rn18_single_scale_nni/', natural=False, store_path='./graphs/nni_cityscapes_0.5_poisoning')
 
     # get_final_epoch_stats(model_name='SwiftNet, Natural data', data_path='./results/75-23_rn18_single_scale_cs/', natural=True)
